12f78f66366933fcfcba6a740e40b09d2fdf3906
Sam Brunno <sam.brunno@isobar.com>
2021-03-21 16:30:41 +1100

    added badges for projects

--- src/scripts/cms/fields/ArticleFields.js
+++ src/scripts/cms/fields/ArticleFields.js
@@ -1,6 +1,7 @@
 export default [
   {label: "Publish Date", name: "date", widget: "datetime"},
   {label: "Intro Blurb", name: "description", widget: "text"},
+  {label: "Project Type", name: "project_type", widget: "string"},
   {
     label: "Image", name: "image", widget: "image", required: true,
   },
