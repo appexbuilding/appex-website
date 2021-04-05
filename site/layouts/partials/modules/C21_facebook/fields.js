e274b5fc2e769ffd6242934fe2e1e48f8163fcca
Sam Brunno <sam.brunno@isobar.com>
2021-04-05 11:09:40 +1000

    adding facebook component

---
+++ site/layouts/partials/modules/C21_facebook/fields.js
@@ -0,0 +1,8 @@
+export default {
+  label: "Facebook",
+  name: "C21_facebook",
+  widget: "object",
+  fields: [{
+    label: "Facebook Source", name: "facebook", widget: "string", required: false, default: "",
+  }],
+};
